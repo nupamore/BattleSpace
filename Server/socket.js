eaf56d07adce81821ccc2381c0e330a704ccd26f
Sangbaek Lee <nupamore@gmail.com>
2017-10-30 16:11:05 +0900

    Update socket.js

--- Server/socket.js
+++ Server/socket.js
@@ -1,12 +1,12 @@
-﻿const Io = require('socket.io')
+const Io = require('socket.io')
 
 const actors = {}
 
-const rank = {
-  '1': { kill: 0, death: 0 },
-  '2': { kill: 0, death: 0 },
-  '3': { kill: 0, death: 0 },
-  '4': { kill: 0, death: 0 },
+const stat = {
+  red: { kill: 0, death: 0 },
+  green: { kill: 0, death: 0 },
+  blue: { kill: 0, death: 0 },
+  yellow: { kill: 0, death: 0 },
 }
 
 
@@ -17,6 +17,7 @@ module.exports = function createSocket(http, m) {
     console.log('new connection')
     actors[actor.id] = actor
 
+    // 플레이어 접속
     actor.on('join player', (name) => {
       console.log(`join ${name}`)
       actor.status = {
@@ -24,10 +25,11 @@ module.exports = function createSocket(http, m) {
         type: 'player'
       }
 
-      rank[name].kill = 0
-      rank[name].death = 0
+      stat[name].kill = 0
+      stat[name].death = 0
     })
 
+    // 상대방 접속
     actor.on('join enemy', (name) => {
       actor.status = {
         name,
@@ -35,33 +37,57 @@ module.exports = function createSocket(http, m) {
       }
     })
 
+    // 플레이어 조작
     actor.on('player controlled', (data) => {
       actor.broadcast.emit('enemy controlled', data)
-      // io.sockets.emit('enemy controlled', data)
     })
 
+    // 플레이어 상태
     actor.on('player status', (data) => {
       actor.broadcast.emit('enemy status', data)
-      // io.sockets.emit('enemy status', data)
     })
 
+    // 플레이어 부활
     actor.on('player respawn', (data) => {
       io.sockets.emit('enemy respawn', data)
     })
 
+    // 플레이어 사망
     actor.on('player dead', (data) => {
-      rank[data.id].death++
-      rank[data.killed].kill++
-      console.log(rank)
+      stat[data.id].death++
+      stat[data.killed].kill++
     })
 
+    // 접속 해제
     actor.on('disconnect', () => {
       console.log('disconnected ' + actor.id)
       delete actors[actor.id]
     })
-
+    
+    // 랭크 뿌려주기
     setInterval(() => {
-      io.sockets.emit('rank', rank)
+      let rank = []
+
+      for(const name in stat) {
+        rank.push({
+          id: name,
+          kill: stat[name].kill,
+          death: stat[name].death
+        })
+      }
+
+      rank = rank.sort((a, b) => {
+        if(a.kill > b.kill) return -1
+        if(a.kill < b.kill) return 1
+        else return 0
+      })
+
+      const result = {}
+      for(const n in rank) {
+        result[n*1+1] = rank[n]
+      }
+
+      io.sockets.emit('rank', result)
     }, 1000)
   })
 }
